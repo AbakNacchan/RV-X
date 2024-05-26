755918636d932e97d038c7cd5b36d69c827a56dd
AbakNacchan <153147835+AbakNacchan@users.noreply.github.com>
2024-05-27 05:14:44 +0700

    Changed the CLI source

--- build.sh
+++ build.sh
@@ -24,7 +24,7 @@ DEF_INTEGRATIONS_VER=$(toml_get "$main_config_t" integrations-version) || DEF_IN
 DEF_CLI_VER=$(toml_get "$main_config_t" cli-version) || DEF_CLI_VER=""
 DEF_PATCHES_SRC=$(toml_get "$main_config_t" patches-source) || DEF_PATCHES_SRC="anddea/revanced-patches"
 DEF_INTEGRATIONS_SRC=$(toml_get "$main_config_t" integrations-source) || DEF_INTEGRATIONS_SRC="anddea/revanced-integrations"
-DEF_CLI_SRC=$(toml_get "$main_config_t" cli-source) || DEF_CLI_SRC="j-hc/revanced-cli"
+DEF_CLI_SRC=$(toml_get "$main_config_t" cli-source) || DEF_CLI_SRC="inotia00/revanced-cli"
 DEF_RV_BRAND=$(toml_get "$main_config_t" rv-brand) || DEF_RV_BRAND="RV-X"
 mkdir -p $TEMP_DIR $BUILD_DIR
 
